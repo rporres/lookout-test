82b5c33db3c3775eb70ea0b758509e79e460a6c2
Carlos Martín <carlosms@users.noreply.github.com>
2018-08-31 15:29:48 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -5,3 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+// A long line to force dummy analyzer comment; A long line to force dummy analyzer comment; A long line to force dummy analyzer comment; 

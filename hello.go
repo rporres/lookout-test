20f92aab8098fe63a9695264e905d6ae06f83ca9
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-10-02 15:35:01 +0200

    Some changes

--- hello.go
+++ hello.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// A long comment a long comment a long comment a long comment a long comment a long comment a long comment a long comment a long comment
+
 func main() {
 	fmt.Println("hello world")
+
+	return
+
+	a := 5
 }

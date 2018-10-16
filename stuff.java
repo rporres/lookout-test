a5c5e31d912a8864971a81065d3e082bafed57b3
Carlos Martín <carlosms@users.noreply.github.com>
2018-10-16 19:24:04 +0200

    Update stuff.java

--- stuff.java
+++ stuff.java
@@ -1,5 +1,5 @@
 // Taken from https://github.com/bblfsh/sonar-checks/blob/master/bblfsh_sonar_checks/fixtures/java/RSPEC-1214.java
 interface Status {
-   int OPEN = 1;
+   int OPEN = 7;
    int CLOSED = 2;
 }
